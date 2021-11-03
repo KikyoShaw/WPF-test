f5f6b7da33c02f48850b1749a11861e7b8bf05f2
shaw <1939484341@qq.com>
2021-11-03 15:34:03 +0800

    WPF 绑定(Binding) 学习 示例

---
+++ WpfApp/BindingTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingTest
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.dataText.DataContext = new Text() { Name = "WPF" };
+
+			//为当前窗口构建数据上下文
+			this.DataContext = new MainWindowModel();
+		}
+
+		public class Text
+		{
+			public string Name { get; set; }
+		}
+	}
+}
---
+++ WpfApp/BindingTest/MainWindowModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//MVVM模式
+//INotifyPropertyChanged用来通知前端控件，ModelName改变后修改前端显示
+namespace BindingTest
+{
+	public class MainWindowModel: INotifyPropertyChanged
+	{
+		public MainWindowModel()
+		{
+			ModelName = "WPFNAME";
+			//通过线程改变ModelName
+			Task.Run(async() =>
+			{
+				await Task.Delay(3000);
+				ModelName = "NOWPF";
+			});
+		}
+
+		private string modelName;
+		public string ModelName 
+		{
+			get { return modelName; } 
+			set
+			{
+				modelName = value;
+				OnPropertyChanged("ModelName");
+			}
+		}
+
+		//为属性添加通知
+		private void OnPropertyChanged(string propertyName)
+		{
+			if(PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); 
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
