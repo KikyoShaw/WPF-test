cd88815581d872fc9e79744adce12dd86f26a885
shaw <1939484341@qq.com>
2021-11-03 15:51:58 +0800

    按钮 Command 绑定

--- WpfApp/BindingTest/MainWindowModel.cs
+++ WpfApp/BindingTest/MainWindowModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,23 +11,29 @@ using System.Threading.Tasks;
 //INotifyPropertyChanged用来通知前端控件，ModelName改变后修改前端显示
 namespace BindingTest
 {
-	public class MainWindowModel: /*INotifyPropertyChanged*/ ViewModelBase
+	public class MainWindowModel : /*INotifyPropertyChanged*/ ViewModelBase
 	{
 		public MainWindowModel()
 		{
 			ModelName = "WPFNAME";
 			//通过线程改变ModelName
-			Task.Run(async() =>
+			Task.Run(async () =>
 			{
 				await Task.Delay(3000);
 				ModelName = "NOWPF";
 			});
+
+			//按钮
+			SaveCommand = new RelayCommand(() =>
+			{
+				ModelName = "changed by button";
+			});
 		}
 
 		private string modelName;
-		public string ModelName 
+		public string ModelName
 		{
-			get { return modelName; } 
+			get { return modelName; }
 			set
 			{
 				modelName = value;
@@ -34,6 +41,9 @@ namespace BindingTest
 				RaisePropertyChanged();
 			}
 		}
+
+		//按钮绑定
+		public RelayCommand SaveCommand { get; private set; }
 
 		////为属性添加通知
 		//private void OnPropertyChanged(string propertyName)
